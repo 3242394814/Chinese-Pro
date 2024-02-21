07ac7e81135258f3b0c40ef0292acbe2fa10fecf
BBGoat <bbgoat@vip.qq.com>
2024-02-22 01:29:17 +0800

    更新MOD版本号

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
 name = "Chinese++ Pro"
-version = "3.5.1"
-description = "当前版本：" ..version .. "\n更新日期：2024年2月18日\n" ..
+version = "3.6"
+description = "当前版本：" ..version .. "\n更新日期：2024年2月22日\n" ..
 [[
 
 汉化你的客户端/服务器 MOD名称、MOD介绍、MOD设置 甚至MOD内容！
